d1579ebce7ca3287daacf6fd4e5dbeee43339ba4
take-itou <t-007.lp670-4sv@ezweb.ne.jp>
2017-05-26 16:15:59 +0900

    Thinkabl　を追加。

--- app/src/main/java/com/example/ito/taiki/secondkadai/Human.java
+++ app/src/main/java/com/example/ito/taiki/secondkadai/Human.java
@@ -4,22 +4,20 @@ package com.example.ito.taiki.secondkadai;
 import android.util.Log;
 
 class Human extends Animal implements Thinkable {
-    // クラス変数
     String hobby;
 
-    // コンストラクタ
     public Human(String name, int age, String hobby) {
         this.name = name;
         this.age = age;
         this.hobby = hobby;
     }
 
-    // メンバ関数
     public void say() {
-        Log.d("javatest", this.name + "(" + this.age + "歳)" + "「私の名前は〜〜です。年は〜〜歳です。」");
+        Log.d("javatest", "の名前は" + this.name + "です。");
+        Log.d("javatest", "私の年齢は" + this.age + "歳です。");
     }
-    public void move() {
-        Log.d("javatest", this.name + "(" + this.age + "歳)" + "「私は〜〜について考える。");
+    public void think() {
+        Log.d("javatest", "私は" + this.hobby + "について考える。");
     }
 }
 
--- app/src/main/java/com/example/ito/taiki/secondkadai/MainActivity.java
+++ app/src/main/java/com/example/ito/taiki/secondkadai/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ito.taiki.secondkadai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
 
         Human human = new Human("taiki ito", 20, "車");
 
-        human.move();
+    human.say();
+            Log.d("javatest", "の名前は" + this.name + "です。");
+            Log.d("javatest", "私の年齢は" + this.age + "歳です。");
+        human.think();
+        Log.d("javatest", "私は" + this.hobby + "について考える。");
+
     }
 }
